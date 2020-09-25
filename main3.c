3e1993f8c8bb02c133518471f696720665d6bed4
fareed0915 <51469715+fareed0915@users.noreply.github.com>
2020-09-24 21:35:22 -0400

    Create main3.c

---
+++ main3.c
@@ -0,0 +1,34 @@
+
+# include  <stdio.h>  
+# include <string.h>  
+# include <sys/types.h>  
+# include <stdlib.h>  
+
+void main ( void ){  
+ int pid;  
+ int i;  
+ srand ( time ( NULL ));  
+ 
+ for (i = 0 ; i < 2 ;++i){  
+ pid = fork ();  
+ 
+ if (pid == 0 ){  
+ int range_loop = ( rand () % 30 ) + 1 ;  
+ int range_sleep = ( rand () % 10 ) + 1 ;  
+ int i;  
+ for (i = 0 ; i < range_loop; i++){  
+ printf ( "Child PID: %d is going to sleep!\n" , getpid ());   sleep (range_sleep);  
+ printf ( "Child %d : I'm awake!\n Where is my Parent  
+%d ?\n" , getpid (), getppid ());  
+ }  
+ exit ( 0 );  
+ }  
+ else if (pid > 0 ){  
+ int status = wait (&status); 
+ printf ( "Child Pid: %d has completed!\n" ,status);   }  
+ else {  
+ printf ( "Something is wrong!" );  
+ exit ( 0 );  
+ }  
+ }  
+}
